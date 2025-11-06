e654c0c36d2daa47d21359b25de4b5f0e48ebb09
JM Kim <kjmhs501@naver.com>
2025-11-07 00:15:55 +0900

    chore: 마이그레이션 초기화 후 food_category에 초기데이터 추가

--- 제이엠_김정민/prisma/migrations/20251106132927_chore_user_gender/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `gender` on the `user` table. The data in that column could be lost. The data in that column will be cast from `VarChar(10)` to `Enum(EnumId(0))`.
-
-*/
--- AlterTable
-ALTER TABLE `user` MODIFY `gender` ENUM('MALE', 'FEMALE', 'OTHER') NOT NULL;
--- 제이엠_김정민/prisma/migrations/20251106151228_insert_food_category/migration.sql
+++ 제이엠_김정민/prisma/migrations/20251106151228_insert_food_category/migration.sql
@@ -4,7 +4,6 @@ CREATE TABLE `address` (
     `detail_address` VARCHAR(255) NOT NULL,
     `region_id` INTEGER NOT NULL,
 
-    INDEX `idx_address_region_id`(`region_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -26,8 +25,6 @@ CREATE TABLE `comment` (
     `comment_content` TEXT NOT NULL,
     `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
-    INDEX `idx_comment_review_id`(`review_id`),
-    INDEX `idx_comment_user_id`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -54,12 +51,13 @@ CREATE TABLE `mission` (
 CREATE TABLE `point` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER NOT NULL,
+    `mission_id` INTEGER NOT NULL,
     `point_amount` INTEGER NOT NULL,
     `type` VARCHAR(50) NOT NULL,
     `description` VARCHAR(255) NULL,
     `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
-    INDEX `idx_point_user_id`(`user_id`),
+    UNIQUE INDEX `point_user_id_key`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -78,11 +76,10 @@ CREATE TABLE `restaurant` (
 -- CreateTable
 CREATE TABLE `restaurant_food_map` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `food_id` INTEGER NOT NULL,
+    `food_category_id` INTEGER NOT NULL,
     `restaurant_id` INTEGER NOT NULL,
 
-    INDEX `idx_rfm_restaurant_id`(`restaurant_id`),
-    UNIQUE INDEX `uq_rfm_food_restaurant`(`food_id`, `restaurant_id`),
+    UNIQUE INDEX `uq_rfm_food_restaurant`(`food_category_id`, `restaurant_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -91,7 +88,6 @@ CREATE TABLE `restaurant_mission_map` (
     `restaurant_id` INTEGER NOT NULL,
     `mission_id` INTEGER NOT NULL,
 
-    INDEX `idx_rmm_mission_id`(`mission_id`),
     PRIMARY KEY (`restaurant_id`, `mission_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -104,8 +100,6 @@ CREATE TABLE `review` (
     `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `rating` DECIMAL(2, 1) NOT NULL,
 
-    INDEX `idx_review_restaurant_id`(`restaurant_id`),
-    INDEX `idx_review_user_id`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -116,15 +110,15 @@ CREATE TABLE `user` (
     `email` VARCHAR(255) NOT NULL,
     `phone_number` VARCHAR(20) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
-    `gender` VARCHAR(10) NOT NULL,
+    `gender` ENUM('MALE', 'FEMALE', 'OTHER') NOT NULL,
     `birth` DATE NOT NULL,
-    `create_at` DATETIME(0) NOT NULL,
-    `update_at` DATETIME(0) NOT NULL,
-    `status` VARCHAR(20) NULL,
+    `create_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `update_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `status` ENUM('ACTIVE', 'INACTIVE', 'SUSPENDED', 'DELETED') NOT NULL DEFAULT 'ACTIVE',
     `password` VARCHAR(255) NOT NULL,
 
+    UNIQUE INDEX `user_address_id_key`(`address_id`),
     UNIQUE INDEX `email`(`email`),
-    INDEX `FK_User_Address`(`address_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -133,7 +127,6 @@ CREATE TABLE `user_favor_category` (
     `user_id` INTEGER NOT NULL,
     `food_category_id` INTEGER NOT NULL,
 
-    INDEX `fk_ufc_food`(`food_category_id`),
     PRIMARY KEY (`user_id`, `food_category_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -146,8 +139,6 @@ CREATE TABLE `user_mission` (
     `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `is_active` BOOLEAN NOT NULL DEFAULT true,
 
-    INDEX `idx_um_mission_id`(`mission_id`),
-    INDEX `idx_um_user_id`(`user_id`),
     UNIQUE INDEX `uq_user_mission`(`user_id`, `mission_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -156,5 +147,68 @@ CREATE TABLE `user_mission` (
 ALTER TABLE `address` ADD CONSTRAINT `address_region_id_fkey` FOREIGN KEY (`region_id`) REFERENCES `region`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `comment` ADD CONSTRAINT `comment_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment` ADD CONSTRAINT `comment_review_id_fkey` FOREIGN KEY (`review_id`) REFERENCES `review`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `point` ADD CONSTRAINT `point_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `point` ADD CONSTRAINT `point_mission_id_fkey` FOREIGN KEY (`mission_id`) REFERENCES `mission`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `restaurant` ADD CONSTRAINT `restaurant_address_id_fkey` FOREIGN KEY (`address_id`) REFERENCES `address`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+-- AddForeignKey
+ALTER TABLE `restaurant_food_map` ADD CONSTRAINT `restaurant_food_map_food_category_id_fkey` FOREIGN KEY (`food_category_id`) REFERENCES `food_category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `restaurant_food_map` ADD CONSTRAINT `restaurant_food_map_restaurant_id_fkey` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `restaurant_mission_map` ADD CONSTRAINT `restaurant_mission_map_restaurant_id_fkey` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `restaurant_mission_map` ADD CONSTRAINT `restaurant_mission_map_mission_id_fkey` FOREIGN KEY (`mission_id`) REFERENCES `mission`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `review` ADD CONSTRAINT `review_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `review` ADD CONSTRAINT `review_restaurant_id_fkey` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user` ADD CONSTRAINT `user_address_id_fkey` FOREIGN KEY (`address_id`) REFERENCES `address`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_favor_category` ADD CONSTRAINT `user_favor_category_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_favor_category` ADD CONSTRAINT `user_favor_category_food_category_id_fkey` FOREIGN KEY (`food_category_id`) REFERENCES `food_category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_mission` ADD CONSTRAINT `user_mission_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_mission` ADD CONSTRAINT `user_mission_mission_id_fkey` FOREIGN KEY (`mission_id`) REFERENCES `mission`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- DDL (테이블 구조 변경) 구문 아래에 추가합니다.
+
+-- FoodCategory 초기 데이터 (Seed Data) 삽입
+INSERT INTO food_category (food_type) VALUES
+('한식'),
+('일식'),
+('중식'),
+('양식'),
+('치킨'),
+('분식'),
+('고기/구이'),
+('도시락'),
+('야식 (족발,보쌈)'),
+('패스트푸드'),
+('디저트'),
+('아시아안푸드');
+
+-- 데이터베이스와 Prisma 스키마의 컬럼 이름에 맞게 food_category와 food_type을 사용했습니다.
